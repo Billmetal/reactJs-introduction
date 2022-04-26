8ea546f36218a279edae1111ad3e2e5147a66ac9
Billmetal <billmetal9@gmail.com>
2022-04-26 14:04:12 -0300

    Instalado e configurado o Webpack e Babel .

---
+++ teste.js
@@ -0,0 +1,10 @@
+/** Campos originais da config scripts no package.json */
+
+// "start": "react-scripts start",
+    // "build": "react-scripts build",
+    // "test": "react-scripts test",
+    // "eject": "react-scripts eject"  
+
+
+
+console.log("Hello World");
