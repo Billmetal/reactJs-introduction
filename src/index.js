e7554fc015877ef9c242d2693734591660314990
Billmetal <billmetal9@gmail.com>
2022-04-26 16:49:47 -0300

    Chaves e Listas .

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ function alerta(a,b){
   alert(sum(a,b));
 }
 
+/****** JSX  *******/
+
 function primeiroJSX() {
     return (<div>
       Willian K. - Introdução ao ReactJs
@@ -20,12 +22,14 @@ function primeiroJSX() {
     </div>);
 }
 
+/****** Conditional-Renderization  *******/
+
 const buttonA = <button>Primeiro Botão</button>
 const buttonB = <button>Segundo Botão</button>
 
 const itemBooleano = true;
 
-const showInTrue = () => (
+const showInTrue = (
   <div>
     Botão a ser mostrado quando true
     <br/>
@@ -33,7 +37,7 @@ const showInTrue = () => (
   </div> 
 );
 
-const showInFalse = () => (
+const showInFalse = (
   <div>
     Botão a ser mostrado quando false
     <br/>
@@ -45,26 +49,81 @@ const showOrNot = () => {
   return itemBooleano ? (<div><h1>Informação ou Componente a ser Mostrado !</h1></div>) : null; 
 };
 
+/****** Chaves e Listas  *******/
+
+const listCustomer = [
+  {
+    id: 1,
+    name: "Bruno Carneiro",
+    skills: ["React","Node","CSS","WebPack"]
+  },
+  {
+    id: 2,
+    name: "Aline Vasconcelos",
+    skills: ["HTML","Javascript","CSS","VueJs"]
+  },
+  {
+    id: 3,
+    name: "Fulano de Tal",
+    skills: ["Java","Spring Boot","Android","Angular"]
+  },{
+    id: 4,
+    name: "José Ciclano",
+    skills: ["C#","Java","Javascript","Python"]
+  }
+];
+
+const renderCustomer = (customer,index) => {
+  return (
+    <div key={`customer-${customer.id}`}>
+      <li>{customer.name}</li>
+      {customer.skills.map(renderSkills)}
+    </div>
+  );
+};
+
+const renderSkills = (skill,index) => {
+  return (
+    <div style={{paddingLeft: "30px"}} key={`skill-${index}`}>
+      <li>{skill}</li>
+    </div>
+  );
+};
+
+/****** Função Principal  *******/
+
 const App = () => {
-  return (<div className='App'>
-    Hello World
-    <br/>
-    {primeiroJSX()}
-    <Button onClick={() => alerta(100,100)} name="Solta Alerta !"></Button>
-    <ComponentA>
-      <ComponentB>
-        <Button onClick={() => alerta(20,50)} name="Solta Alerta Segundo !"></Button>
-      </ComponentB>
-    </ComponentA>
-    <Lifecycle></Lifecycle>
-    <br/>
-    <h2>===========  Continua com Conditional-Renderization ===============</h2>
-    {itemBooleano ? showInTrue() : showInFalse()} 
-    <br/>
-    {showOrNot()}  
+  return (
+    <div className='App'>
+      Hello World
+      <br/>
+      {primeiroJSX()}
+      <Button onClick={() => alerta(100,100)} name="Solta Alerta !"></Button>
+      <ComponentA>
+        <ComponentB>
+          <Button onClick={() => alerta(20,50)} name="Solta Alerta Segundo !"></Button>
+        </ComponentB>
+      </ComponentA>
+      <br/>
+      <h2>===========  Ecossistemas e Lifecycle ===============</h2>
+      <Lifecycle></Lifecycle>
+      <br/>
+      <h2>===========  Continua com Conditional-Renderization ===============</h2>
+      {itemBooleano ? showInTrue : showInFalse} 
+      <br/>
+      {showOrNot()} 
+      <br/>
+      <h2>===========  Chaves e Listas ===============</h2> 
+      <div>
+        <ul>
+          {listCustomer.map(renderCustomer)}
+        </ul>
+      </div>
     </div>
   );  
 };
+
+/****** Classe para Ecossistemas e Lifecycle  *******/
 
 class Lifecycle extends Component {
 
